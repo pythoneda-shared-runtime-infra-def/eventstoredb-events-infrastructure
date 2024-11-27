77c507868b774eae707623ae674b8a0a6c18fb7b
Jose San Leandro Armendáriz <github@acm-sl.org>
2024-11-27 18:02:49 +0100

    Commit created with sync-pythoneda.sh

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       inputs.nixos.follows = "nixos";
       inputs.pythoneda-shared-pythonlang-banner.follows =
         "pythoneda-shared-pythonlang-banner";
-      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.86";
+      url = "github:pythoneda-shared-pythonlang-def/domain/0.0.87";
     };
     pythoneda-shared-pythonlang-infrastructure = {
       inputs.flake-utils.follows = "flake-utils";
@@ -51,7 +51,7 @@
       inputs.pythoneda-shared-pythonlang-domain.follows =
         "pythoneda-shared-pythonlang-domain";
       url =
-        "github:pythoneda-shared-runtime-infra-def/eventstoredb-events/0.0.10";
+        "github:pythoneda-shared-runtime-infra-def/eventstoredb-events/0.0.11";
     };
   };
   outputs = inputs:
@@ -60,8 +60,8 @@
       let
         org = "pythoneda-shared-runtime-infra";
         repo = "eventstoredb-events-infrastructure";
-        version = "0.0.2";
-        sha256 = "1wbk5a0sl7sfby6w4hbczw9aw119ph9w13vryjm2wb0cz18p2f5k";
+        version = "0.0.3";
+        sha256 = "0wvx3cagwgr4kwfr2bj6fh8afzidw9adsyhhgcxasa28bkzf5z4s";
         pname = "${org}-${repo}";
         pythonpackage =
           "pythoneda.shared.runtime.infrastructure.events.infrastructure.eventstoredb";
